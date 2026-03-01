d40e44e46147e2cc13394c9cd8b5b105d142fa5e
antares <antares@alyr.dev>
2026-03-01 16:23:19 +0800

    Fix Intel rate limiting

--- devshell/sde.nix
+++ devshell/sde.nix
@@ -20,7 +20,7 @@ stdenv.mkDerivation {
     "fixupPhase"
   ];
   src = builtins.fetchTarball {
-    url = "https://downloadmirror.intel.com/843185/sde-external-9.48.0-2024-11-25-lin.tar.xz";
+    url = "https://github.com/Antares0982/ssrjson-nix-dev/releases/download/v0.0.0/sde-external-9.48.0-2024-11-25-lin.tar.xz";
     sha256 = "sha256:1z9nd3lfixwm0nyxim7x7vgfkmxxzj608lacqwm983cbw1x2dg04";
   };
 }
